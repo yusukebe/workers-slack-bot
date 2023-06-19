748ce106a7291cc98e6aea81849e682e407f77c4
Yusuke Wada <yusuke@cloudflare.com>
2023-06-19 18:49:11 +0900

    check response

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -21,11 +21,13 @@ app.post('/', async (c) => {
 	const issue_string = `${repository.owner.login}/${repository.name}#${issue.number}`;
 	const blocks = constructGhIssueSlackMessage(issue, issue_string, prefix_text);
 
-	await fetch(c.env.SLACK_WEBHOOK_URL, {
+	const fetchResponse = await fetch(c.env.SLACK_WEBHOOK_URL, {
 		body: JSON.stringify({ blocks }),
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 	});
+
+	if (!fetchResponse.ok) throw new Error();
 
 	return c.text('OK');
 });
